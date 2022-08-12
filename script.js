4637b7c647ae813664ac2219aa0c8104ee001c11
Codeyash1999 <codeyash1999@gmail.com>
2022-08-12 09:49:04 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.query.addEventListener("click", function() {
+		circle.style.width = "400px";
+		circle.style.height = "400px";
+		circle.innerHTML = "Your Business Growth Partner";
+		clicked = true;
+	}
+);
